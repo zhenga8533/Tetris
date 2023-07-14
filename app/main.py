aac22c6d98d428c1be08505420b2bccc6dc12914
Allen Zheng <volcaronitee8533@gmail.com>
2023-07-14 12:13:38 -0400

    Updated organization and UI

--- app/main.py
+++ app/main.py
@@ -1,30 +1,15 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 
-class Board(GridLayout):
-    def __init__(self, **kwargs):
-        super(Board, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="Name: "))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
-
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-
-    def press(self, instance):
-        print("Boop!")
+class Grid(Widget):
+    pass
 
 
 class Tetris(App):
     def build(self):
-        return Board()
+        return Grid()
 
 
 if __name__ == "__main__":
