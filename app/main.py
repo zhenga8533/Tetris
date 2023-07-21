5030954bd5500b6ef1d18109bf307daefd757df4
Allen Zheng <volcaronitee8533@gmail.com>
2023-07-21 17:51:19 -0400

    Resemblance of something

--- app/main.py
+++ app/main.py
@@ -1,33 +1,58 @@
 import random
+
+import numpy
 from kivy.app import App
 from kivy.core.audio import SoundLoader
+from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
-from block import Block
+from kivy.vector import Vector
+from kivy.core.window import Window
+
+# Kivy Game PogO
+Window.size = (Window.size[1] / 2, Window.size[1])
 
 
-class TetrisWidget(Widget):
+class TetrisBlock(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.board = [[(255, 255, 255) for i in range(10)] for j in range(20)]
-        self.block = Block(random.randint(0, 6))
+        # Block Variables
+        self.size = (Window.size[0] / 10, Window.size[1] / 20)
+
+        self.pos = (Window.size[0] / 2, Window.size[1])
+        self.vel_x = NumericProperty(0)
+        self.vel_y = NumericProperty(-5)
+        self.velocity = ReferenceListProperty(self.vel_x, self.vel_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class TetrisGame(Widget):
+    block = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.board = numpy.zeros((20, 10))
         self.delay = 1
 
         # Load Music
         self.sound = SoundLoader.load("assets/bgm.wav")
         self.sound.play()
 
-        # Set Clock
-        Clock.schedule_interval(self.play_tick, self.delay)
+    def get_block(self, vel=(0, -30)):
+        self.block.velocity = vel
 
     def play_tick(self, _):
-        self.block.move([0, -1], self.board)
-        print(self.board)
+        self.block.move()
 
 
 class TetrisApp(App):
     def build(self):
-        return TetrisWidget()
+        game = TetrisGame()
+        game.get_block()
+        Clock.schedule_interval(game.play_tick, 1)
+        return game
 
 
 if __name__ == "__main__":
